0f59878964c9c6fe9c401cb3b1c82aed91689afa
danijcc <69958834+danijcc@users.noreply.github.com>
2023-01-25 19:08:45 -0500

    test4

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -1,7 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <?php//aqui esriimos el contenido que queremos que se repita en todas las paginas?>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
